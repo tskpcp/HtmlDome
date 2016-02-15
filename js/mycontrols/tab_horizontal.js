e9be43139be1409e0a07c973a5b6f5d41a13f4af
tskpcp <tskpcp@126.com>
2016-02-15 16:15:42 +0800

    Revert "add"
    This reverts commit d5367f9d7c7ea52f6fdad8a228d8e1d3d2a3db12.

--- js/mycontrols/tab_horizontal.js
+++
@@ -1,4 +0,0 @@
-$(".tab_title_box li").click(function(){
-	$(this).addClass("tab_title_cur").siblings("li").removeClass("tab_title_cur");
-	$(".tab_box").find(".tab_box_centent").hide().eq($(this).index()).show();
-});
