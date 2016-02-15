54b7ba2a8b7b59efc759c3754b91d8f3ec492901
tskpcp <tskpcp@126.com>
2016-02-15 17:34:02 +0800

    update index

---
+++ js/index.js
@@ -0,0 +1,4 @@
+$(function(){
+	$("nav").css({height:$(window).height()-$("header").height()-$("footer").height()});
+	$("section").css({height:$(window).height()-$("header").height()-$("footer").height()});
+});
