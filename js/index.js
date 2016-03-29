42405553cbc885b0c468cabee14d1d6eee15ba5d
tskpcp <tskpcp@126.com>
2016-03-29 17:15:26 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,19 +1,63 @@
 $(function(){
-	var hederHeight=$("header").height();
-	var footerHeight=$("footer").height();
-	var windowHeight=$(window).height();
-	var navHeight=$("nav").height();
-	var contentHeight=0;
-	var tempHeight=0;
-	tempHeight=windowHeight-hederHeight-footerHeight;
-	if(tempHeight>navHeight){
-		contentHeight=tempHeight;
-	}else{
-		contentHeight=navHeight;
-	}
-	$("nav").css({height:contentHeight});
-	$("section").css({height:contentHeight});
-	$(".tooltip-tip").click(function(){
-		alert("aa");
-	});
+//	var hederHeight=$("header").height();
+//	var footerHeight=$("footer").height();
+//	var windowHeight=$(window).height();
+//	var windowWidth=$(window).width();
+//	var menuHeight=$("#skin-select").height();
+//	var menuWidth=$("#skin-select").width();	
+//	$("section").css({"height":windowHeight-hederHeight-footerHeight});
+});
+head.js("js/skin-select/skin-select.js");
+head.js("js/custom/scriptbreaker-multiple-accordion-1.js", function() {
+
+    $(".topnav").accordionze({
+        accordionze: true,
+        speed: 500,
+        closedSign: '<img src="img/plus.png">',
+        openedSign: '<img src="img/minus.png">'
+    });
+});
+//TOOL TIP
+head.js("js/tip/jquery.tooltipster.js", function() {
+    $('.tooltip-tip-x').tooltipster({
+        position: 'right'
+    });
+    $('.tooltip-tip').tooltipster({
+        position: 'right',
+        animation: 'slide',
+        theme: '.tooltipster-shadow',
+        delay: 1,
+        offsetX: '-12px',
+        onlyOne: true
+    });
+    $('.tooltip-tip2').tooltipster({
+        position: 'right',
+        animation: 'slide',
+        offsetX: '-12px',
+        theme: '.tooltipster-shadow',
+        onlyOne: true
+    });
+    $('.tooltip-top').tooltipster({
+        position: 'top'
+    });
+    $('.tooltip-right').tooltipster({
+        position: 'right'
+    });
+    $('.tooltip-left').tooltipster({
+        position: 'left'
+    });
+    $('.tooltip-bottom').tooltipster({
+        position: 'bottom'
+    });
+    $('.tooltip-reload').tooltipster({
+        position: 'right',
+        theme: '.tooltipster-white',
+        animation: 'fade'
+    });
+    $('.tooltip-fullscreen').tooltipster({
+        position: 'left',
+        theme: '.tooltipster-white',
+        animation: 'fade'
+    });
+    //For icon tooltip
 });
