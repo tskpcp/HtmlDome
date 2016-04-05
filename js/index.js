9f7e6f4c5d30854b60ddd0865c56287558bfde82
tskpcp <tskpcp@126.com>
2016-04-05 18:08:44 +0800

    update 4-5

--- js/index.js
+++ js/index.js
@@ -1,57 +1,13 @@
 $(function(){
-
-});
-head.js("js/skin-select/skin-select.js");
-head.js("js/custom/scriptbreaker-multiple-accordion-1.js", function() {
-
-    $(".topnav").accordionze({
-        accordionze: true,
-        speed: 500,
-        closedSign: '<img src="img/plus.png">',
-        openedSign: '<img src="img/minus.png">'
-    });
-});
-//TOOL TIP
-head.js("js/tip/jquery.tooltipster.js", function() {
-    $('.tooltip-tip-x').tooltipster({
-        position: 'right'
-    });
-    $('.tooltip-tip').tooltipster({
-        position: 'right',
-        animation: 'slide',
-        theme: '.tooltipster-shadow',
-        delay: 1,
-        offsetX: '-12px',
-        onlyOne: true
-    });
-    $('.tooltip-tip2').tooltipster({
-        position: 'right',
-        animation: 'slide',
-        offsetX: '-12px',
-        theme: '.tooltipster-shadow',
-        onlyOne: true
-    });
-    $('.tooltip-top').tooltipster({
-        position: 'top'
-    });
-    $('.tooltip-right').tooltipster({
-        position: 'right'
-    });
-    $('.tooltip-left').tooltipster({
-        position: 'left'
-    });
-    $('.tooltip-bottom').tooltipster({
-        position: 'bottom'
-    });
-    $('.tooltip-reload').tooltipster({
-        position: 'right',
-        theme: '.tooltipster-white',
-        animation: 'fade'
-    });
-    $('.tooltip-fullscreen').tooltipster({
-        position: 'left',
-        theme: '.tooltipster-white',
-        animation: 'fade'
-    });
-    //For icon tooltip
+	var htemp='<ul>';
+	for(i=1;i<=10;i++){
+		htemp+='<li><a><span>'+i+'</span></a></li>';
+	}
+	htemp+='</ul>';	
+	$('.navSubBox').html(htemp);
+	
+	$(".navBox-nth2 li a span").click(function(){
+		alert($(this).html());
+	
+	});
 });
